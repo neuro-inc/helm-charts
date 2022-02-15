7633dfbe43a6aee1169c398bf1ea0fb5406575db
Ivan Zubenko <vanya.zubenko@gmail.com>
2022-02-15 17:18:38 +0200

    add rollout deployment option to acme chart

--- charts/acme/files/acme.sh
+++ charts/acme/files/acme.sh
@@ -16,6 +16,8 @@ _notify_hook=""
 _force_option=""
 _secret=()
 _secret_namespace="default"
+_rollout_deployment=""
+_rollout_deployment_namespace="default"
 
 _acme_install() {
   curl https://get.acme.sh | sh -s email="$_email"
@@ -40,10 +42,20 @@ _acme_issue() {
     acme.sh --set-notify --notify-hook "$_notify_hook"
   fi
 
+  _renew_hook="$_script install-cert $_debug_option -d ${_domains[0]}"
+
+  if [ ! -z "$_secret" ]; then
+    _renew_hook="$_renew_hook --secret $_secret --secret-namespace $_secret_namespace"
+  fi
+
+  if [ ! -z "$_rollout_deployment" ]; then
+    _renew_hook="$_renew_hook --rollout-deployment $_rollout_deployment --rollout-deployment-namespace $_rollout_deployment_namespace"
+  fi
+
   acme.sh --issue \
     --dns dns_$_dns \
     --server $_server \
-    --renew-hook "$_script install-cert $_debug_option -d ${_domains[0]} --secret $_secret --secret-namespace $_secret_namespace" \
+    --renew-hook "$_renew_hook" \
     $_debug_option \
     $_force_option \
     $_domain_options \
@@ -51,18 +63,16 @@ _acme_issue() {
 
   $_script install-cert \
     $_debug_option \
-    -d ${_domains[0]}\
-    --secret $_secret \
-    --secret-namespace $_secret_namespace
+    -d "${_domains[0]}" \
+    --secret "$_secret" \
+    --secret-namespace "$_secret_namespace" \
+    --rollout-deployment "$_rollout_deployment" \
+    --rollout-deployment-namespace "$_rollout_deployment_namespace"
 }
 
 _acme_install_cert() {
   if [ ${#_domains[@]} == 0 ]; then
     echo "Domain is required."
-    exit 1
-  fi
-  if [ -z "$_secret" ]; then
-    echo "Secret is required."
     exit 1
   fi
 
@@ -77,15 +87,21 @@ _acme_install_cert() {
     --fullchain-file $_path/fullchain.pem \
     --key-file $_path/key.pem
 
-  kubectl create secret generic $_secret \
-    -n $_secret_namespace \
-    --from-file=cert.crt=$_path/fullchain.pem \
-    --from-file=cert.key=$_path/key.pem \
-    --save-config \
-    --dry-run \
-    -o yaml \
-    2>/dev/null | \
-  kubectl apply -f -
+  if [ ! -z "$_secret" ]; then
+    kubectl create secret generic $_secret \
+      -n $_secret_namespace \
+      --from-file=cert.crt=$_path/fullchain.pem \
+      --from-file=cert.key=$_path/key.pem \
+      --save-config \
+      --dry-run \
+      -o yaml \
+      2>/dev/null | \
+    kubectl apply -f -
+  fi
+
+  if [ ! -z "$_rollout_deployment" ]; then
+    kubectl rollout restart deployment/$_rollout_deployment -n $_rollout_deployment_namespace
+  fi
 }
 
 shift # past cmd
@@ -118,7 +134,7 @@ while [[ $# -gt 0 ]]; do
       shift # past argument
       shift # past value
       ;;
-    --notify)
+    --notify-hook)
       _notify_hook="$2"
       shift # past argument
       shift # past value
@@ -134,6 +150,16 @@ while [[ $# -gt 0 ]]; do
       ;;
     --secret-namespace)
       _secret_namespace="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --rollout-deployment)
+      _rollout_deployment="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --rollout-deployment-namespace)
+      _rollout_deployment_namespace="$2"
       shift # past argument
       shift # past value
       ;;
